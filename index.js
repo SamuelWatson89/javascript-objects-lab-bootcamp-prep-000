1442219b8ece722717128d8b6dbc2d462ce1f9d8
Samuel Watson <otacon100@hotmail.com>
2018-06-29 13:36:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(recipes, banana, peel){
-  return Object.assign({}, recipes, { [banana]: peel })
-  
   console.log(recipes)
+  return Object.assign({}, recipes, { [banana]: peel })
 }
 
