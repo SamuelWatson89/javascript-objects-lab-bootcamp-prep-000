3743a72261a394701931254e4f106ba47385d3f4
Samuel Watson <otacon100@hotmail.com>
2018-06-29 13:40:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(recipes, cake, flour){
   return Object.assign({}, recipes, { [cake]: flour })
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(recipe, Ccke, flour) {
+function destructivelyUpdateObjectWithKeyAndValue(recipe, Cake, flour) {
   delete recipes[cake]
   return recipes
 }
